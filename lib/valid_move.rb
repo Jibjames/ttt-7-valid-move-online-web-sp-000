973116c3b3761387958cb67e5956b88fbb5af0e5
James Bridges <masterbridges@gmail.com>
2019-12-04 18:07:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,9 @@
 # code your #valid_move? method here
 def valid_move(board, index)
   space = board[index]
-  
+# must move to a position within the board
+# position must be vacant
+
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
