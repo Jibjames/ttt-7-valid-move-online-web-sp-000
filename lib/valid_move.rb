2d544ae200c4f4e60e2a1531848ccae6031772ae
James Bridges <masterbridges@gmail.com>
2019-12-04 18:04:47 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,15 @@
 # code your #valid_move? method here
+def valid_move(board, index)
+  space = board[index]
+  
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  space = board[index]
+  if space == " " || space == "" || space == NIL
+    return FALSE
+  else 
+    return TRUE
+  end
+end
